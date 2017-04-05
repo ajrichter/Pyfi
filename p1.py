3a281b6cef155b18049746afcabe6482cc4871e9
ajrichter <ajrichter@knox.edu>
2017-04-05 13:32:10 -0500

    P2

--- p1.py
+++ p1.py
@@ -5,9 +5,14 @@ import pandas as pd
 import pandas_datareader.data as web
 
 style.use('ggplot')
+#start = dt.datetime(2000,1,1)
+#end = dt.datetime(2016,12,31)
+#df = web.DataReader('TSLA', 'yahoo', start, end)
+#print(df.tail(6))
+#df.to_csv('tesla.csv')
+df = pd.read_csv('tesla.csv', parse_dates=True, index_col=0)
+#print(df.head())
 
-start = dt.datetime(2000,1,1)
-end = dt.datetime(2016,12,31)
-
-df = web.DataReader('TSLA', 'yahoo', start, end)
-print(df.tail(6))
+print(df[['Open' , 'High']].head())
+df['Adj Close'].plot()
+plt.show()
