fced0bad3eec6d8b744ab86d2f21998e27d2f80c
ajrichter <ajrichter@knox.edu>
2017-04-23 21:35:09 -0500

    SP500 correlation tracking

---
+++ testmutfund.py
@@ -0,0 +1,12 @@
+# I am going to test getting example mut fund data
+import bs4 as bs
+import datetime as dt
+import matplotlib.pyplot as plt
+from matplotlib import style
+import os
+import pandas as pd
+import pandas_datareader.data as web
+import pickle
+import requests
+
+style.use('ggplot')
