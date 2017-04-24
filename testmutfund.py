7689cbb1ec5037c6fc2aec3c68c94f91da43ecbe
ajrichter <ajrichter@knox.edu>
2017-04-23 21:38:13 -0500

    testing Mutual Fund data access

--- testmutfund.py
+++ testmutfund.py
@@ -8,5 +8,11 @@ import pandas as pd
 import pandas_datareader.data as web
 import pickle
 import requests
+import csv
 
 style.use('ggplot')
+
+with open('mutual.csv', 'rb') as cf:
+        spamreader = csv.reader(cf, delimiter=',', quotechar='|')
+        for row in spamreader:
+            print ', '.join(row)
